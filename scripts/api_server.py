4e24eb046a31edca7f4da3466301580caca2b682
Daniel D <s222311422@deakin.edu.au>
2023-09-25 15:19:42 +1000

    removed duplicate file

--- scripts/api_server.py
+++ scripts/api_server.py
@@ -2,23 +2,31 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# Sample data (will be dynamically retrieved and stored in corresponding dictionary)
-users = [{"id": 1, "name": "John Doe"}, {"id": 2, "name": "Jane Smith"}]
+# Sample weather data (replace with dynamic data retrieval)
+weather_data = [
+    {"location": "New York", "date": "2023-09-17", "temperature": 25.0, "humidity": 60.0},
+    {"location": "New York", "date": "2023-09-18", "temperature": 26.0, "humidity": 65.0},
+    # Add more weather data as needed
+]
 
-# Example endpoint data request of all data under 'users'
-@app.route('/users', methods=['GET'])
-def get_users():
-    return jsonify(users)
+# Endpoint to get weather data for a specific location
+@app.route('/weather/<location>', methods=['GET'])
+def get_weather_by_location(location):
+    result = [weather for weather in weather_data if weather['location'].lower() == location.lower()]
+    return jsonify(result)
 
-# Example endpoint data request (with direct specification)
-@app.route('/users/<int:user_id>', methods=['GET'])
+# Endpoint to get weather data for a specific date
+@app.route('/weather/<location>/<date>', methods=['GET'])
+def get_weather_by_location_and_date(location, date):
+    result = [weather for weather in weather_data if weather['location'].lower() == location.lower() and weather['date'] == date]
+    return jsonify(result)
 
-# Creation of example endpoints, adjust as necessary depending on data type retrieval
-def get_user(user_id):
-    user = next((user for user in users if user['id'] == user_id), None)
-    if user is None:
-        return jsonify({'message': 'User not found'}), 404
-    return jsonify(user)
+# Endpoint to get weather data for a date range
+@app.route('/weather/<location>/<start_date>/<end_date>', methods=['GET'])
+def get_weather_by_location_and_date_range(location, start_date, end_date):
+    result = [weather for weather in weather_data if
+              weather['location'].lower() == location.lower() and start_date <= weather['date'] <= end_date]
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run(debug=True)
